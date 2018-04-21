0c00dc33136f69b934430a82332d05ace56198e4
Chahat Goel <chahatgoel98@gmail.com>
2018-04-21 19:53:42 +0530

    connecting it to server

--- app/src/main/java/com/chahatgoel/scanner/MainActivity.java
+++ app/src/main/java/com/chahatgoel/scanner/MainActivity.java
@@ -1,26 +1,89 @@
 package com.chahatgoel.scanner;
 
+import android.app.ProgressDialog;
+import android.app.VoiceInteractor;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.Result;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
     private ZXingScannerView zXingScannerView;
+    Button login;
+
+
+    private RequestQueue requestQueue;
+    private StringRequest stringRequest;
+    public static final String TAG="a";
+
+
+    String url = new String();
+    ProgressDialog pd;
+    String response = new String();
+
+    String result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendrequest();
+            }
+        });
+
+
+
+
+
     }
+
+    private void sendrequest() {
+        requestQueue=Volley.newRequestQueue(this
+
+        );
+        stringRequest= new StringRequest(Request.Method.GET,getString(R.string.server)+ "/parking/89/31131" , new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.i(TAG, "onResponse: "+ response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(stringRequest);
+
+    }
+
+
     public void scan (View view)
     { zXingScannerView= new ZXingScannerView(getApplicationContext());
     setContentView(zXingScannerView);
     zXingScannerView.setResultHandler(this);
     zXingScannerView.startCamera();
+
     }
 
     @Override
@@ -33,5 +96,10 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     public void handleResult(Result result) {
         Toast.makeText(getApplicationContext(),result.getText(),Toast.LENGTH_SHORT).show();
         zXingScannerView.resumeCameraPreview(this);
+
+
+
+
     }
+
 }
